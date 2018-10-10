537115ca3844d9b53ec9345f39756f333f74278f
Rakesh Chauhan <chauhan.rakoo7570@gmail.com>
2018-10-10 10:59:06 +0530

    adding login and signup template

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').strategy;
+var multer = require('multer');
+var upload = multer({ dest: './uploads'});
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var usersignin = require('./routes/auth/login');
+var usersignup = require('./routes/auth/signup');
 
 var app = express();
 
@@ -13,14 +25,52 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//handle sessions
+app.use(session({
+  secret:'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg ,value){
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+    while(namespace.length){
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value:value
+    };
+  }
+}));
+
+app.use(require('connect-flash')());
+app.use(function(req,res,next){
+  res.locals.messages = require('express-messages')(req,res);
+  next();
+});
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth/login', usersignin);
+app.use('/auth/signup', usersignup);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
